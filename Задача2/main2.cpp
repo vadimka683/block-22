8b1b91afcc0d39020f276c2f85917f97d90dcbef
vivasuk683 <vivas@DESKTOP-BE431TS>
2025-03-24 15:02:27 +0300

    Убрал multimap

--- Задача2/main2.cpp
+++ Задача2/main2.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-void load(multimap<string, int>& queue) {
+void load(map<string, int>& queue) {
 	ifstream load("Queue.BIN", ios::binary);
 	if (!load.is_open()) {
 		cout << "Error load queue.\n";
@@ -22,39 +22,60 @@ void load(multimap<string, int>& queue) {
 		if (!load.read(&temp[0], temp_lenght)) {
 			break;
 		}
+		if (!load.read(reinterpret_cast<char*>(&count), sizeof(count))) {
+			break;
+		}
 		queue.insert(make_pair(temp, count));
 	}
 }
 
-void save(multimap<string, int>& queue) {
+void save(map<string, int>& queue) {
 	ofstream save("Queue.BIN", ios::binary);
 	for (map<string, int>::iterator it = queue.begin(); it != queue.end(); it++) {
 		string surname = it->first;
 		size_t surname_lenght = surname.length();
 		save.write(reinterpret_cast<char*>(&surname_lenght), sizeof(surname_lenght));
 		save.write(surname.data(), surname_lenght);
+		save.write(reinterpret_cast<char*>(&it->second), sizeof(it->second));
 	}
 }
 
-void print_queue(multimap<string, int>& queue) {
+void print_queue(map<string, int>& queue) {
 	for (const auto& It : queue) {
-		cout << It.first << "\n";
+		for (int i = 0; i < It.second; i++) {
+			cout << It.first << "\n";
+		}
 	}
 }
 
-void pushback_queue(multimap<string, int>& queue, string& surname, int& count) {
-	count++;
-	queue.insert(make_pair(surname, count));
+void pushback_queue(map<string, int>& queue, string& surname) {
+	map<string, int>::iterator it = queue.find(surname);
+	if (it == queue.end()) {
+		int count = 1;
+		queue.insert(make_pair(surname, count));
+	}
+	else {
+		it->second++;
+	}
 }
 
-void next(multimap<string, int>& queue) {
+void next(map<string, int>& queue) {
 	map<string, int>::iterator itf = queue.begin();
+	if (itf == queue.end()) {
+		cout << "Queue is empty\n";
+		return;
+	}
 	cout << itf->first << "\n";
-	queue.erase(itf);
+	if (itf->second > 1) {
+		itf->second--;
+	}
+	else {
+		queue.erase(itf);
+	}
 }
 
 int main() {
-	multimap<string, int> queue;
+	map <string, int> queue;
 	int count_queue = 0;
 	while (true) {
 		cout << "Input command: ";
@@ -76,7 +97,7 @@ int main() {
 			return 0;
 		}
 		else {
-			pushback_queue(queue, command, count_queue);
+			pushback_queue(queue, command);
 		}
 	}
 }
